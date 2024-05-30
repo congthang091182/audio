763140d0bbd0c5641d33bd8bd3539b0b35fe9f11
congthang091182 <167980900+congthang091182@users.noreply.github.com>
2024-05-30 16:00:25 +0700

    Add files via upload

--- LePhatDong.py
+++ LePhatDong.py
@@ -6,6 +6,8 @@ import datetime as dt
 import time
 import os
 import openai
+import plotly.express as px
+#import matplotlib.pyplot as plt
 #import altair as alt
 #Connect to database
 db_user = st.secrets["db_credentials"]["user"]
@@ -25,12 +27,34 @@ st.header(':orange[Gửi tiền tiết kiệm, chung tay vì người nghèo]', 
 #st.header('_Streamlit_ is :blue[cool] :sunglasses:')
 #//
 #cac ham xuly input
+button_style = """
+<style>
+div.stButton > button:first-child {
+    background-color: #4CAF50;  /* Green background */
+    color: white;  /* White text */
+    border: none;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    transition-duration: 0.4s;
+    cursor: pointer;
+}
+
+div.stButton > button:first-child:hover {
+    background-color: white;
+    color: black;
+    border: 2px solid #4CAF50;
+}
+</style>
+"""
 ##clear txt
 #
 st.title('Đăng ký thông tin Khách hàng')
 cursor = cnxn.cursor()
 title = st.text_input(":red[Họ và tên (*)]","")
-
 #title = st.text_input(":orange[Enter your text here]")
 today = dt.datetime.now()
 start_date = dt.datetime(1900, 1, 1,)
@@ -66,6 +90,7 @@ combobox = st.selectbox(
 )
 title6 = st.text_input("Đơn vị công tác", "")
 st.write(":red[(*)]  bắt buộc")
+st.markdown(button_style, unsafe_allow_html=True)
 click=st.button("Đồng ý")
 if click:
   d= d.strftime("%x")
@@ -83,6 +108,7 @@ if click:
   #st.button("Clear", on_click=on_click)
   st.success('Cảm ơn bạn đã đăng ký')
 #nếu click xem
+st.header(':white[Thống kê số liệu]', divider='rainbow')
 click0=st.button("Xem số lượng khách hàng đã đăng ký")
 if click0:
   para = ()
@@ -101,18 +127,37 @@ if click0:
                     columns=[column_info[0] for column_info in cursor.description],
                     coerce_float=True
                 )
+  para1 = ()
+  query1  = "select DONVI N'Đơn vị công tác',COUNT(CCCD) N'Số khách hàng' from LEPHATDONG where HOTEN is not null and TOCHUCHOI is not null GROUP BY DONVI order by DONVI"
+  data1=cursor.execute(query1,para1).fetchall()
+  df1 = pd.DataFrame.from_records(
+                    data=data1,
+                    columns=[column_info[0] for column_info in cursor.description],
+                    coerce_float=True
+                )
   #st.write(df)
 # chart = alt.Chart(df).mark_bar().encode(
 #         alt.X("SOKH", bin=True),
 #         y='count()',
 #     )
-  tab1, tab2 = st.tabs(["Theo đơn vị ủy thác", "Theo nơi gửi"])
+  tab1, tab2, tab3 = st.tabs(["Theo đơn vị ủy thác", "Theo nơi gửi","Theo đơn vị công tác"])
   with tab1:
           st.write(df, theme="streamlit", use_container_width=True)
+          fig = px.bar(df, x='Tổ chức hội', y='Số khách hàng', title='Biểu đồ số lượng tổ chức hội', color=['red', 'green', 'blue','orange'])
+
+          # Displaying the Plotly chart in Streamlit
+          st.plotly_chart(fig)
   with tab2:
           st.write(df0, theme=None, use_container_width=True)
+          fig = px.bar(df0, x='Nơi gửi', y='Số khách hàng', title='Biểu đồ số lượng POS')
+          st.plotly_chart(fig)
+  with tab3:
+          st.write(df1, theme=None, use_container_width=True)
+          fig = px.line(df1, x='Đơn vị công tác', y='Số khách hàng', title='Biểu đồ theo ĐVCT')
+          st.plotly_chart(fig)
 #connect close
 #cursor.close()
+   
 cnxn.close()
 
 
