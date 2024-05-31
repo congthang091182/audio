47b183db40c674fd075db43de54830fef493a15b
congthang091182 <167980900+congthang091182@users.noreply.github.com>
2024-05-31 16:08:47 +0700

    Add files via upload

--- LePhatDong.py
+++ LePhatDong.py
@@ -111,8 +111,42 @@ if click:
 st.header(':white[Thống kê số liệu]', divider='rainbow')
 click0=st.button("Xem số lượng khách hàng đã đăng ký")
 if click0:
-  colors = ['red', 'blue', 'green', 'yellow']
-  
+  colors = ['red', 'blue', 'green', 'yellow','aqua','azure','azure','beige','black','brown','cyan','darkblue','darkcyan','darkgreen','darkkhaki'
+            ,'darkmagenta','darkolivegreen','darkorchid','darkred','darksalmon','darkviolet','fuchsia','gold','indigo','lightblue','navy']
+#   Colors.names = {
+   
+#     darkmagenta: "#8b008b",
+#     darkolivegreen: "#556b2f",
+#     darkorange: "#ff8c00",
+#     darkorchid: "#9932cc",
+#     darkred: "#8b0000",
+#     darksalmon: "#e9967a",
+#     darkviolet: "#9400d3",
+#     fuchsia: "#ff00ff",
+#     gold: "#ffd700",
+#     green: "#008000",
+#     indigo: "#4b0082",
+#     khaki: "#f0e68c",
+#     lightblue: "#add8e6",
+#     lightcyan: "#e0ffff",
+#     lightgreen: "#90ee90",
+#     lightgrey: "#d3d3d3",
+#     lightpink: "#ffb6c1",
+#     lightyellow: "#ffffe0",
+#     lime: "#00ff00",
+#     magenta: "#ff00ff",
+#     maroon: "#800000",
+#     navy: "#000080",
+#     olive: "#808000",
+#     orange: "#ffa500",
+#     pink: "#ffc0cb",
+#     purple: "#800080",
+#     violet: "#800080",
+#     red: "#ff0000",
+#     silver: "#c0c0c0",
+#     white: "#ffffff",
+#     yellow: "#ffff00"
+# };
   para = ()
   query  = "select TOCHUCHOI N'Tổ chức hội',COUNT(CCCD) N'Số khách hàng' from LEPHATDONG where HOTEN is not null and TOCHUCHOI is not null GROUP BY TOCHUCHOI"
   
