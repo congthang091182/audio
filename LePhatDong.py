155ba62a31f06b707feb5c860786e03e2f5621c7
congthang091182 <167980900+congthang091182@users.noreply.github.com>
2024-06-04 09:28:19 +0700

    Add files via upload

--- LePhatDong.py
+++ LePhatDong.py
@@ -7,26 +7,18 @@ import time
 import os
 import openai
 import plotly.express as px
-#import matplotlib.pyplot as plt
-#import altair as alt
-#Connect to database
 db_user = st.secrets["db_credentials"]["user"]
 db_password = st.secrets["db_credentials"]["password"]
 db_host = st.secrets["db_credentials"]["host"]
 db_database = st.secrets["db_credentials"]["Database"]
 openai.api_key=st.secrets["db_credentials"]["api_key"]
-#st.write("db_credentials abc:", openai.api_key)
+        #st.write("db_credentials abc:", openai.api_key)
 cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};'
                       f'SERVER={db_host};'
                       f'DATABASE={db_database};'
                       f'UID={db_user};'
                       f'PWD={db_password}')
-  #username=st.secrets.db_credentials.username, password=st.secrets.db_credentials.password)
-#//connect open
-st.header(':orange[Gửi tiền tiết kiệm, chung tay vì người nghèo]', divider='rainbow')
-#st.header('_Streamlit_ is :blue[cool] :sunglasses:')
-#//
-#cac ham xuly input
+cursor = cnxn.cursor()
 button_style = """
 <style>
 div.stButton > button:first-child {
@@ -50,27 +42,38 @@ div.stButton > button:first-child:hover {
 }
 </style>
 """
-##clear txt
-#
-st.title('Đăng ký thông tin Khách hàng')
-cursor = cnxn.cursor()
-title = st.text_input(":red[Họ và tên (*)]","")
-#title = st.text_input(":orange[Enter your text here]")
-today = dt.datetime.now()
-start_date = dt.datetime(1900, 1, 1,)
-#st.button("Subit", type="primary")
-d = st.date_input("Ngày tháng năm sinh", min_value=start_date,max_value= today , format="DD-MM-YYYY",)
-title0 = st.text_input("Số CMT (nếu có)", "")
-title1 = st.text_input(":red[Số CCCD(*)]", "")
-d0 = st.date_input("Ngày cấp", today , format="DD-MM-YYYY",)
-title2 = st.text_input("Nơi cấp", "Cục cảnh sát QLHC về TTXH")
-title3 = st.text_input(":red[Địa chỉ thường trú(*)]", "")
-title4 = st.text_input(":red[Số điện thoại(*)]", "")
-sotien=st.number_input("Nhập số tiền gửi ",format="%f")
-st.write(f"Giá trị:  :green[{sotien:,}]")
-#st.write("Combining **bold and :green[colored text] is totally** fine! Just like with other markdown features.")
-#sotien=sotien.format("%f")
-combobox0 = st.selectbox(
+st.header(':orange[Gửi tiền tiết kiệm, chung tay vì người nghèo]', divider='rainbow')
+st.markdown("""
+            <style>
+            .css-d1b1ld.edgvbvh6
+            {
+                visibilyti: hidden;
+            }
+            .css-1v8iw7l.eknhn3m4;
+            {
+                 visibilyti: hidden;
+            }
+            </style>
+            """,unsafe_allow_html=True)
+st.subheader("Đăng ký thông tin khách hàng")
+with st.form("form2"):
+    today = dt.datetime.now()
+    start_date = dt.datetime(1900, 1, 1,)
+    col1,col2=st.columns(2)
+    title=col1.text_input(":red[Họ và tên (*)]","")
+    d=col2.date_input("Ngày tháng năm sinh", min_value=start_date,max_value= today , format="DD-MM-YYYY",)
+    col3,col4=st.columns(2)
+    title0=col3.text_input("Số CMT cũ (nếu có)")
+    title1=col4.text_input(":red[Số CCCD(*)]", "")
+    col5,col6=st.columns(2)
+    d0=col5.date_input ("Ngày cấp", today , format="DD-MM-YYYY",)
+    title2=col6.text_input("Nơi cấp","Cục cảnh sát QLHC về TTXH")
+    col7,col8=st.columns(2)
+    title4=col7.text_input (":red[Số điện thoại(*)]", "")
+    sotien=col8.number_input("Số tiền gửi",format="%f")
+    #st.write(f"Giá trị:  :green[{sotien:,}]")
+    #col9=st.columns(1)
+    combobox0 = st.selectbox(
    ":red[Nơi gửi tiết kiệm (*)]",
    ("Thành phố", "Cao Lộc", "Lộc Bình"
         ,"Đình Lập", "Văn Lãng", "Tràng Định",
@@ -80,7 +83,10 @@ combobox0 = st.selectbox(
    index=None,
    placeholder="click để chọn...",
 )
-combobox = st.selectbox(
+    #col10=st.columns(1)
+    title3=st.text_input(":red[Địa chỉ thường trú(*)]", "")
+    #col11=st.columns(1)
+    combobox = st.selectbox(
    "Đơn vị ủy thác",
    ("Hội nông dân", "Hội phụ nữ","Hội cựu chiến binh", "Đoàn thanh niên"
         ,"Khác"
@@ -88,65 +94,28 @@ combobox = st.selectbox(
    index=None,
    placeholder="click để chọn...",
 )
-title6 = st.text_input("Đơn vị công tác", "")
-st.write(":red[(*)]  bắt buộc")
-st.markdown(button_style, unsafe_allow_html=True)
-click=st.button("Đồng ý")
-if click:
-  d= d.strftime("%x")
-  d0= d0.strftime("%x")
-  para = (title,d,title0,title1,d0,title2,title3,title4,sotien,combobox,title6,combobox0)
-  query=""" insert into lephatdong (HOTEN,NGAYSINH,CMT_CU,CCCD,NGAYCAP,NOICAP,DIACHI,DIENTHOAI,SOTIEN,TOCHUCHOI,DONVI,POS) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)"""
-  #query  = "exec LEPHATDONG_INSERT @HOTEN=?,@NGAYSINH=?,@CMT_CU=?,@CCCD=?,@NGAYCAP=?,@DIACHI=?,@DIENTHOAI=?,@SOTIEN=?,@TOCHUCHOI=?,@DONVI=?"
-  cursor.execute(query,para)
-  def clear_text():
-    title = st.empty()
-    title0 = st.empty()
-  cnxn.commit()
-  clear_text()
-  
-  #st.button("Clear", on_click=on_click)
-  st.success('Cảm ơn bạn đã đăng ký')
-#nếu click xem
+    #col12=st.columns(1)
+    title6=  st.text_input("Đơn vị công tác")
+    st.click=st.form_submit_button("Đồng ý")
+    if st.click:
+        d= d.strftime("%x")
+        d0= d0.strftime("%x")
+        para = (                          title,d,title0,title1,d0,title2,title3,title4,sotien,combobox,title6,combobox0)
+        query=""" insert into lephatdong (HOTEN,NGAYSINH,CMT_CU,CCCD,NGAYCAP,NOICAP,DIACHI,DIENTHOAI,SOTIEN,TOCHUCHOI,DONVI,POS) VALUES(?,?,?,?,?,?,?,?,?,?,?,?)"""
+            #query  = "exec LEPHATDONG_INSERT @HOTEN=?,@NGAYSINH=?,@CMT_CU=?,@CCCD=?,@NGAYCAP=?,@DIACHI=?,@DIENTHOAI=?,@SOTIEN=?,@TOCHUCHOI=?,@DONVI=?"
+        cursor.execute(query,para)
+        def clear_text():
+                title = st.empty()
+                title0 = st.empty()
+        cnxn.commit()
+        clear_text()
+        st.success('Cảm ơn bạn đã đăng ký')
 st.header(':white[Thống kê số liệu]', divider='rainbow')
 click0=st.button("Xem số lượng khách hàng đã đăng ký")
 if click0:
   colors = ['virginica', 'blue','red', 'green', 'yellow','aqua','azure','azure','beige','black','brown','cyan','darkblue','darkcyan','darkgreen','darkkhaki'
             ,'darkmagenta','darkolivegreen','darkorchid','darkred','darksalmon','darkviolet','fuchsia','gold','indigo','lightblue','navy']
-#   Colors.names = {
-   
-#     darkmagenta: "#8b008b",
-#     darkolivegreen: "#556b2f",
-#     darkorange: "#ff8c00",
-#     darkorchid: "#9932cc",
-#     darkred: "#8b0000",
-#     darksalmon: "#e9967a",
-#     darkviolet: "#9400d3",
-#     fuchsia: "#ff00ff",
-#     gold: "#ffd700",
-#     green: "#008000",
-#     indigo: "#4b0082",
-#     khaki: "#f0e68c",
-#     lightblue: "#add8e6",
-#     lightcyan: "#e0ffff",
-#     lightgreen: "#90ee90",
-#     lightgrey: "#d3d3d3",
-#     lightpink: "#ffb6c1",
-#     lightyellow: "#ffffe0",
-#     lime: "#00ff00",
-#     magenta: "#ff00ff",
-#     maroon: "#800000",
-#     navy: "#000080",
-#     olive: "#808000",
-#     orange: "#ffa500",
-#     pink: "#ffc0cb",
-#     purple: "#800080",
-#     violet: "#800080",
-#     red: "#ff0000",
-#     silver: "#c0c0c0",
-#     white: "#ffffff",
-#     yellow: "#ffff00"
-# };
+
   para = ()
   #query  = "select TOCHUCHOI N'Tổ chức hội',COUNT(CCCD) N'Số khách hàng' from LEPHATDONG where HOTEN is not null and TOCHUCHOI is not null GROUP BY TOCHUCHOI"
   query  = "exec get_LPD_TOCHUCHOI"
@@ -185,7 +154,7 @@ if click0:
           #adjusted_colors = colors[:len("Tổ chức hội")]
           st.write(df, theme="streamlit", use_container_width=True)
           color_discrete_map = {'virginica': 'blue', 'setosa': 'red', 'versicolor': 'green'}
-          fig = px.bar(df, x='Tổ chức hội', y='Số khách hàng', title='Biểu đồ số lượng tổ chức hội', color=colors[:len(df['MA'])])#, color=['red', 'green', 'blue','orange'])
+          fig = px.bar(df, x='Tổ chức hội', y='Số khách hàng', title='Biểu đồ số lượng tổ chức hội', color=colors[:len(df['Tổ chức hội'])])#, color=['red', 'green', 'blue','orange'])
 
           # Displaying the Plotly chart in Streamlit
           st.plotly_chart(fig)
@@ -199,8 +168,9 @@ if click0:
           st.plotly_chart(fig)
 #connect close
 #cursor.close()
-   
+  # with st.form("form2"):
+  #   col1,col2=st.columns(2)
+  #   col1.text_input("Họ và tên")
+  #   col2.text_input("Năm sinh")
+  #   st.form_submit_button("Đồng ý")
 cnxn.close()
-
-
-#st.text_input('Enter text here:', key='widget', on_change=clear_text)
