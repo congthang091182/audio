863ea47ce6c7d15824c2e4c259c4d8380b94a6a3
congthang091182 <167980900+congthang091182@users.noreply.github.com>
2024-06-06 10:24:21 +0700

    Add files via upload

--- LePhatDong.py
+++ LePhatDong.py
@@ -197,4 +197,13 @@ if click0:
   #   col2.text_input("Năm sinh")
   #   st.form_submit_button("Đồng ý")
 st.image('bbb.jpg', caption='Sản phẩm tiền gửi')
+with st.form("form3",clear_on_submit=True):
+    st.url = "https://www.facebook.com/profile.php?id=100083312079198"
+    st.url1 = "https://vbsp.org.vn/gioi-thieu/lai-suat-huy-dong.html?"
+#st.write("check out this [link](%s)" % url)
+    st.markdown("fanpage facebook NHCSXH Tỉnh Lạng sơn Lạng Sơn: [click here](%s)" % st.url)
+    st.markdown("Xem thêm sản phẩm tiền gửi: [click here](%s)" % st.url1)
+    st.click=st.form_submit_button("Thanks!")
+    if st.click:
+            st.markdown("Nếu có lỗi gì liên hệ: 0988.649.666")
 cnxn.close()
